2baea12b2fcd9fc7803f3eced18dca7406ff1fbf
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-14 22:22:29 +0200

    Initial commit.

---
+++ OLIO/Teht3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna kolme lukua : ");
+            string rivi = Console.ReadLine();
+            int luku1, luku2, luku3;
+            Console.WriteLine("Summa on " + (luku1 + luku2 + luku3));
+            Console.WriteLine("Keskiarvo on ")
+        }
+    }
+}
