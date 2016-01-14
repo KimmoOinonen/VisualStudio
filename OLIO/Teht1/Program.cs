2d53f419fbce7fbd3498ccf049269ecedcab75e5
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-14 22:07:48 +0200

    Initial commit.

---
+++ OLIO/Teht1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // kysytään käyttäjältä luku
+            Console.Write("Anna luku : ");
+            string rivi = Console.ReadLine(); // "1"
+            //int luku = int.Parse(rivi); // 1
+            int luku;
+            bool onnistuiko = int.TryParse(rivi, out luku);
+            if (onnistuiko)
+            {
+
+                // tulostetaan luku "sanana"
+                switch (luku)
+                {
+                    case 1: Console.WriteLine("Yksi"); break;
+                    case 2: Console.WriteLine("Kaksi"); break;
+                    case 3: Console.WriteLine("Kolme"); break;
+                    default: Console.WriteLine("Joku muu luku"); break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Et antanut lukua!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
