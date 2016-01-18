a88807a34da92dc32ce4cf53fb2230bdc86c8662
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-19 01:14:42 +0200

    Initial commit.

---
+++ OLIO/Teht6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distance;
+            double consumption, price;
+            Console.Write("Anna kuljettu matka : ");
+            string rivi = Console.ReadLine();
+            int.TryParse(rivi, out distance);
+
+            consumption = (7.02 / 100) * distance;
+            price = consumption * 1.595;
+
+            Console.Write("Bensaa kuluu {0:F2} litraa,", consumption);
+            Console.Write(" kustannukset ovat {0:F2} euroa", price);
+
+            Console.ReadLine();
+        }
+    }
+}
