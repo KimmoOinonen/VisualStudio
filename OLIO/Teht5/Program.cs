87dbc3c68b913af47b98f078f30033015e7a95a4
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-19 00:45:35 +0200

    Initial commit.

---
+++ OLIO/Teht5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, hours, minutes, seconds;
+            Console.Write("Anna sekunnit : ");
+            string rivi = Console.ReadLine();
+            int.TryParse(rivi, out number);
+
+            hours = number / 3600;
+            minutes = (number - (hours * 3600)) / 60;
+            seconds = (number - ((hours * 3600) + (minutes * 60)));
+            
+            Console.Write("Sekuntiaika voidaan ilmaista muodossa: ");
+            Console.Write("\nTunnit " + hours);
+            Console.Write("\nMinuutit " + minutes);
+            Console.Write("\nSekunnit " + seconds);
+
+            Console.ReadLine();
+        }
+    }
+}
