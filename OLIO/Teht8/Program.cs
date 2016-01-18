818862494685ab59f248a3df1537fb41917ab7fb
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-19 01:23:22 +0200

    Initial commit.

---
+++ OLIO/Teht8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku1, luku2, luku3;
+            Console.Write("Anna luku : ");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku : ");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku : ");
+            luku3 = int.Parse(Console.ReadLine());
+
+            if (luku1 > luku2 && luku1 > luku3)
+            {
+                Console.WriteLine("Suurin luku on " + luku1);
+            }
+            else if (luku2 > luku1 && luku2 > luku3)
+            {
+                Console.WriteLine("Suurin luku on " + luku2);
+            }
+            else if (luku3 > luku1 && luku3 > luku2)
+            {
+                Console.WriteLine("Suurin luku on " + luku3);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
