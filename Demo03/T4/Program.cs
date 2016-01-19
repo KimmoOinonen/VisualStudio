509b7567b9a90f4aab8d38dc945c3a8da67f812a
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-19 11:13:41 +0200

    Initial commit.

---
+++ Demo03/T4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+            vehicle.Name = "Auto";
+            vehicle.Speed = 100;
+            vehicle.Tyres = 4;
+            vehicle.PrintData();
+            Console.WriteLine(vehicle.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo03/T4/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4
+{
+    class Vehicle
+    {
+        // properties
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        // method prints vehicle data
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle data : ");
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Speed : " + Speed);
+            Console.WriteLine("Tyres : " + Tyres);
+        }
+
+        // method returns heater data
+        public override string ToString()
+        {
+            return "Vehicle : " + Name + " Speed : " + Speed + " Tyres : " +Tyres;
+        }
+    }
+}
