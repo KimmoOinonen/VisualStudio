3764744ebdbbe0cb5b3b22ff62fe3e61155eded9
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-20 00:04:27 +0200

    Initial commit.

---
+++ Demo03/Teht6/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+* Copyright (C) Kimmo Oinonen
+*
+* Homework project in Object-Oriented and GUI Programming.
+* This is little program simulating radiator.
+*
+* Created: 19.1.2016.
+*
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radiator radiator = new Radiator();
+            radiator.On = true;
+            radiator.Off = false;
+            radiator.Power = 5;
+            radiator.RoomTemp = 22;
+            radiator.PrintData();
+
+            Console.ReadLine();
+        }
+    }
+}
