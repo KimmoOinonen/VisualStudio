daa0bfb6ec5963824e0e6bf18ea2888c0bf71e0b
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-20 13:10:04 +0200

    Initial commit.

---
+++ Demo04/CarApplication/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Car
+    {
+        // max speed
+        private readonly int maxSpeed = 200;
+        // member field
+        private int speed;
+        // property
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                if (value <= maxSpeed) speed = value;
+                else
+                {
+                    Console.WriteLine("Too much speed - set to max!");
+                    speed = maxSpeed;
+                }
+            }
+        }
+    }
+}
