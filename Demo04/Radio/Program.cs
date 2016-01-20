8f89cd804560e63c434eb42f975a3ebc0f52c40a
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-20 15:12:45 +0200

    Initial commit.

---
+++ Demo04/Radio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+            radio.OnOff = true;
+            radio.Volume = 7;
+            radio.Frequency = 22000;
+            Console.WriteLine(radio.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo04/Radio/Radio.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    class Radio
+    {
+        public bool OnOff;
+
+        private readonly int maxVolume = 9;
+        private readonly double maxFrequency = 26000.0;
+        private readonly double minFrequency = 2000.0;
+
+        private int volume;
+        private double frequency;
+
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= maxVolume) volume = value;
+                else
+                {
+                    Console.WriteLine("Too much volume - set to max!");
+                    volume = maxVolume;
+                }
+            }
+        }
+
+        public double Frequency
+        {
+            get
+            {
+                return frequency;
+            }
+            set
+            {
+                if (value <= maxFrequency && value >= minFrequency) frequency = value;
+                else
+                {
+                    Console.WriteLine("Too much or too little frequency - set to min!");
+                    frequency = minFrequency;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Radio is on : " + OnOff + " Frequency is : " + Frequency + " Volume is : " + Volume;
+        }
+    }
+}
