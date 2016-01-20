9320b8038f4436c13ac27e7c5eb80ec7d33b16d5
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-20 14:20:17 +0200

    Initial commit.

---
+++ Demo04/PersonApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Kimmo";
+            person.LastName = "Oinonen";
+            person.Address = "Minna Canthin katu";
+            person.Age = 21;
+            person.PhoneNumber = "0407298087";
+            Console.WriteLine(person.ToString());
+
+            Teacher teacher = new Teacher("Jussi", "Konsoli", "D456");
+            teacher.Address = "Piippukatu 2";
+            teacher.Age = 55;
+            teacher.PhoneNumber = "0401234567";
+            Console.WriteLine(teacher.ToString());
+
+            Student student = new Student("Olli", "Opiskelija", "K1744");
+            student.Age = 20;
+            student.Address = "Koulukatu 10";
+            student.PhoneNumber = "0409876543";
+            Console.WriteLine(student.ToString());
+
+
+            Console.WriteLine("Press enter key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo04/PersonApplication/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Student : Person
+    {
+        public string StudentID { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(string firstName, string lastName, string id) 
+            :base(firstName, lastName)
+        {
+            StudentID = id;
+        }
+
+        public void StudentMethod()
+        {
+            Console.WriteLine("Student does something...");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + StudentID;
+        }
+    }
+}
