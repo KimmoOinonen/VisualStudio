95777d23597497a02ea3f16fe991990ecc28dfcb
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-23 22:38:13 +0200

    Initial commit.

---
+++ Demo04/T7/Lights.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7
+{
+    class Lights
+    {
+        public bool OnOff { get; set; }
+        public int BulbWatt { get; set; }
+        public int BulbAmount { get; set; }
+        public string BulbType { get; set; }
+
+        public Lights(bool onOff, string bulbType, int bulbWatt, int bulbAmount)
+        {
+            OnOff = onOff;
+            BulbWatt = bulbWatt;
+            BulbAmount = bulbAmount;
+            BulbType = bulbType;
+        }
+
+        public override string ToString()
+        {
+            return "Is light on or off : " + OnOff + " Type of bulb : " + BulbType + " Watt amount of bulb : " + BulbWatt + "W" + " Amount of bulbs : " + BulbAmount;
+        }
+    }
+}
---
+++ Demo04/T7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lights light1 = new Lights(true, "Led", 4, 3);
+            Console.WriteLine(light1.ToString());
+
+            Lights light2 = new Lights(false, "Halogen", 60, 2);
+            Console.WriteLine(light2.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
